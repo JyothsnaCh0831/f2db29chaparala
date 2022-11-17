d488df11d5139b28abb67ce1c171daf10defe94a
JyothsnaCh0831 <jyothsnach2001@gmail.com>
2022-11-17 13:33:30 -0600

    Done with Part 2 of Lab assignment 13

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var mongoose = require('mongoose');
 var mongodb = require('mongodb');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
 var italianDish = require('./models/italianDish');
 
 // Mongo DB Connection
@@ -41,6 +43,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -49,6 +58,17 @@ app.use('/italianDish', italianDishRouter);
 app.use('/gridbuild', gridBuildRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+ 
+ 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

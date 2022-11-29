91a5334c84810b64cb8088f190b891f9fed7c0e4
JyothsnaCh0831 <jyothsnach2001@gmail.com>
2022-11-28 18:50:10 -0600

    Done with Part 4 of passport assignment

--- routes/italianDish.js
+++ routes/italianDish.js
@@ -2,6 +2,16 @@ var express = require('express');
 const italianDish_controlers= require('../controllers/italianDish'); 
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /* GET Class page. */
 router.get('/', italianDish_controlers.italianDish_view_all_Page);
 
@@ -12,7 +22,7 @@ router.get('/detail', italianDish_controlers.italianDish_view_one_Page);
 router.get('/create', italianDish_controlers.italianDish_create_Page); 
 
 /* GET dish update page */ 
-router.get('/update', italianDish_controlers.italianDish_update_Page); 
+router.get('/update', secured, italianDish_controlers.italianDish_update_Page); 
 
 /* GET delete dish page */ 
 router.get('/delete', italianDish_controlers.italianDish_delete_Page); 

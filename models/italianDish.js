512220bc498e1e3f87fef5b0cc1ba0472b2b3a1d
JyothsnaCh0831 <jyothsnach2001@gmail.com>
2022-11-28 19:38:42 -0600

    Done with Part 5 of passport assignment

--- models/italianDish.js
+++ models/italianDish.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 const italianDishSchema = mongoose.Schema({
-    dishName: String,
-    mainIngredient: String,
-    dishPrice: Number
+    dishName: {
+        type: String,
+        required: [true, 'Name of the dish cannot be empty']
+    },
+    mainIngredient: {
+        type: String,
+        required: [true, 'Ingredient of the dish cannot be empty']
+    },
+    dishPrice: {
+        type: Number,
+        min: [1.25, 'Price must be at least $1.25'],
+        max: 30
+    }
 });
 
 module.exports = mongoose.model("ItalianDish", italianDishSchema);
